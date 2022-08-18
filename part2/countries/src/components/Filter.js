a13ab9f0ef360aabbe1dc3c28a7c103ec48494a1
Baba <babaafrid7@gmail.com>
2022-08-18 22:28:06 +0530

    Files Committed

---
+++ part2/countries/src/components/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Filter = ({ handleFilterChange }) => {
+    return (
+        <div>
+            Find countries:
+            <input onChange={handleFilterChange} />
+        </div>
+    )
+}
+export default Filter
